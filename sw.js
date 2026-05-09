b03000a61617d9e99fb3836b2e18d3596af4e1d4
shadowman5422-cloud <shadowman5422@gmail.com>
2026-05-09 09:01:08 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'hisab-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+            if (res.ok) {
+                const clone = res.clone();
+                caches.open(CACHE).then(c => c.put(e.request, clone));
+            }
+            return res;
+        }))
+    );
+});
